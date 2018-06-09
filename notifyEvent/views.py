727b0d5ab79b4e420d80cd28be03a62e65c8ea2e
Eduardojvr <eduardojvr10@gmail.com>
2018-06-09 11:53:07 -0300

    Configuration send mail

--- notifyEvent/views.py
+++ notifyEvent/views.py
@@ -3,38 +3,45 @@ import urllib, json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import requests
+from email.mime.text import MIMEText
+import smtplib
+
 
 @csrf_exempt
 def sendmail(request, texto, subject, emails):
-    if request.method == "GET":
-        mensagem = MIMEText(texto)
-        mensagem.set_charset('utf-8')
-        mensagem['Subject'] = subject
-        mail = smtplib.SMTP('smtp.gmail.com', 587)
-        mail.ehlo()
-        mail.starttls()
-        mail.login('noreplayfiscae@gmail.com', 'fiscaeunb')
-        mail.sendmail('noreplayfiscae@gmail.com', emails, mensagem.as_string())
+    mensagem = MIMEText(texto)
+    mensagem.set_charset('utf-8')
+    mensagem['Subject'] = subject
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login('noreplayfiscae@gmail.com', 'fiscaeunb')
+    mail.sendmail('noreplayfiscae@gmail.com', emails, mensagem.as_string())
 
-
+@csrf_exempt
 def data_mensage(request):
     email_to = ['noreplayfiscae@gmail.com']
     subject = 'Quadro de horários'
-    text = 'Seu quadro de horário\n'
+    # text = 'Seu quadro de horário\n'
     
     if request.method == "GET":
-        d = requests.get('http://localhost:8000/doctor/api-doctor/?format=json')
-        # data = json.loads(url.read().decode())
-        data = json.loads(d.content)
-
-        # sendmail(request, text, subject, email_to)
-        print("==================================")
-        print(data)
-        print("==================================")
-
+        return HttpResponse("Use post for this action")
     if request.method == "POST":
-        print("==================================")
-        print(data)
-        print("AAAEEEEEEEEEE")
-        print("==================================")
-    
+        d = json.loads(request.body)
+        email = d["email"]
+        segunda = d["segunda"]
+        terca = d["terca"]
+        quarta = d["quarta"]
+        quinta = d["quinta"]
+        sexta = d["sexta"]
+        sabado = d["sabado"]
+        domingo = d["domingo"]
+
+        emailSend = "Segunda: "+segunda+"\n"+"Terça: "+terca+"\n"+"Quarta: "+quarta+"\n"+"Quinta: "+quinta+"\n"+"Sexta: "+sexta+"\n"+"Sábado: "+sabado+"\n"+"Domingo: "+domingo
+        # print("==================================")
+        # print(emailSend)
+        # print("==================================")
+        # sendmail(request, text, subject, email_to)
+        sendmail(request, emailSend, subject, email)        
+        return HttpResponse("Use post for this action")
+
